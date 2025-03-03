97c46da1c4fbf91149b9fa1fa7c03c8758ccec76
derdart <121251970+Derdart@users.noreply.github.com>
2025-03-03 22:09:47 +0200

    Add files via upload

---
+++ src/Image.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Image() {
+  return (
+    <div class="image">
+      <img src="" alt="" />
+      <div class="button">
+        <p id="button">Coming soon...</p>
+      </div>
+    </div>
+  );
+}
+
+export default Image;
